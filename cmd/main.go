dfb6a60f9dcde30ab6d3161c00114af2d1e7772f
Byron Seto <byronseto@hotmail.com>
2023-03-21 23:24:25 -0600

    Makes an empty window

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
 func main() {
-	ebiten.RunGame()
+	ebiten.SetWindowSize(400, 200)
+	ebiten.SetWindowTitle("Hello Sarah")
+	err := ebiten.RunGame(&SimpleGame{})
+	if err != nil {
+		log.Fatalf("unable to run game: %v", err)
+	}
 }
 
 type SimpleGame struct{}
+
+func (s *SimpleGame) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (s *SimpleGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 400, 200
+}
